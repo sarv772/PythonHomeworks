6bc233d324e6b993a0ee270b8a3c572f2d1be93e
Sarvar <savisar772@gmail.com>
2025-03-13 00:07:51 +0500

    Update and rename lesson.py to lesson1.py

---
+++ lesson-1/homework/lesson1.py
@@ -0,0 +1,20 @@
+side = input('Given square side is:')
+perimeter = 4*int(side)
+square = int(side)**2
+print(perimeter)
+print(square)
+diameter = input('Diameter of circle is:')
+lengthofcircle = 3.14*int(diameter)
+print(lengthofcircle)
+a = int(4)
+b = int(8)
+meanofnumbers = float((a+b)/2)
+print(meanofnumbers)
+sum = a + b
+print(sum)
+product = a*b
+print(product)
+squarea = a**2
+print(squarea)
+squareb = b**2
+print(squareb)
