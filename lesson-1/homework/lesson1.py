18d9bf630a1998198ea43ca9b2581e30d28ed248
Sarvar <savisar772@gmail.com>
2025-03-15 10:25:14 +0500

    Update lesson1.py

--- lesson-1/homework/lesson1.py
+++ lesson-1/homework/lesson1.py
@@ -1,15 +1,21 @@
+#1)
 side = input('Given square side is:')
 perimeter = 4*int(side)
 square = int(side)**2
 print(perimeter)
 print(square)
+
+#2)
 diameter = input('Diameter of circle is:')
 lengthofcircle = 3.14*int(diameter)
 print(lengthofcircle)
+
+#3)
 a = int(4)
 b = int(8)
 meanofnumbers = float((a+b)/2)
 print(meanofnumbers)
+#4)
 sum = a + b
 print(sum)
 product = a*b
