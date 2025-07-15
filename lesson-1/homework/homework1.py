e11963df79092ee2ae701269aebcfd2f99597c58
Sarvar <savisar772@gmail.com>
2025-07-15 19:07:42 +0500

    Update homework1.py

--- lesson-1/homework/homework1.py
+++ lesson-1/homework/homework1.py
@@ -1,1 +1,26 @@
+#1)
+side = 8
+perimeter = 4*int(side)
+square = int(side)**2
+print(perimeter)
+print(square)
 
+#2)
+diameter = 4
+lengthofcircle = 3.14*int(diameter)
+print(lengthofcircle)
+
+#3)
+a = int(4)
+b = int(8)
+meanofnumbers = float((a+b)/2)
+print(meanofnumbers)
+#4)
+sum = a + b
+print(sum)
+product = a*b
+print(product)
+squarea = a**2
+print(squarea)
+squareb = b**2
+print(squareb)
